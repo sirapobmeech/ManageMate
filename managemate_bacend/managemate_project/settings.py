912bc8898be4da0ab5e90651e76efa67950a3a34
SirapobGun <sirapobmech@gmail.com>
2019-05-06 22:44:47 +0700

    test

--- managemate_bacend/managemate_project/settings.py
+++ managemate_bacend/managemate_project/settings.py
@@ -116,12 +116,14 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
+
 USE_I18N = True
 
 USE_L10N = True
 
 USE_TZ = True
 
+from .local_setting import *
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
